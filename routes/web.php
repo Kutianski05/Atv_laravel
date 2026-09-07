afb4d3f619523a9755580d935595912fd5851383
João Vitor Kutianski <engs-joaokutianski@camporeal.edu.br>
2026-09-07 20:50:01 -0300

    atv1

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/sobre', function() {
+        return 'Sobre: página com informações gerais do sistema.';
+});
+
+Route::get('/alunos', function() {
+    return 'Alunos: listagem dos alunos cadastrados.';
+});
+
+Route::get('/contato', function() {
+    return 'Contato: fale conosco pelo email contato@email.com';
+});

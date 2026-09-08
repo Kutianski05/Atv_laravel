ff09a95dd3f2bae148680ef8ec781e13d97847d1
engs-jefersonpereira-collab <engs-jefersonpereira@camporeal.edu.br>
2026-09-08 11:18:18 -0300

    atividade 20

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password'])]
+#[Fillable(['name', 'email', 'password', 'role'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
@@ -29,8 +29,14 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
     public function aluno()
     {
-    return $this->hasOne(Aluno::class);
+        return $this->hasOne(Aluno::class);
+    }
+
+    public function isAdmin(): bool
+    {
+        return $this->role === 'admin';
     }
 }

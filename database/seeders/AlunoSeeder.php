088fb95ceb97d2513554005a04e740db91e5ef4f
João Vitor Kutianski <engs-joaokutianski@camporeal.edu.br>
2026-09-07 21:49:50 -0300

    alterações

--- database/seeders/AlunoSeeder.php
+++ database/seeders/AlunoSeeder.php
@@ -10,16 +10,16 @@ class AlunoSeeder extends Seeder
     public function run(): void
     {
         $alunos = [
-            ['nome' => 'João Vitor Kutianski', 'email' => 'joao.kutianski@exemplo.com',  'curso' => 'Engenharia de Software'],
-            ['nome' => 'Maria Silva',          'email' => 'maria.silva@exemplo.com',     'curso' => 'Engenharia de Software'],
-            ['nome' => 'Carlos Silveira',      'email' => 'carlos.silveira@exemplo.com', 'curso' => 'Direito'],
-            ['nome' => 'Ana Paula Souza',      'email' => 'ana.souza@exemplo.com',       'curso' => 'Administração'],
-            ['nome' => 'Pedro Henrique Lima',  'email' => 'pedro.lima@exemplo.com',      'curso' => 'Engenharia Civil'],
-            ['nome' => 'Juliana Costa',        'email' => 'juliana.costa@exemplo.com',   'curso' => 'Engenharia de Software'],
-            ['nome' => 'Rafael Oliveira',      'email' => 'rafael.oliveira@exemplo.com', 'curso' => 'Medicina'],
-            ['nome' => 'Beatriz Almeida',      'email' => 'beatriz.almeida@exemplo.com', 'curso' => 'Direito'],
-            ['nome' => 'Lucas Ferreira',       'email' => 'lucas.ferreira@exemplo.com',  'curso' => 'Administração'],
-            ['nome' => 'Camila Rodrigues',     'email' => 'camila.rodrigues@exemplo.com','curso' => 'Engenharia de Software'],
+            ['nome' => 'João Vitor Kutianski', 'email' => 'joao.kutianski@email.com',  'curso' => 'Engenharia de Software'],
+            ['nome' => 'Maria Silva',          'email' => 'maria.silva@email.com',     'curso' => 'Engenharia de Software'],
+            ['nome' => 'Carlos Silveira',      'email' => 'carlos.silveira@email.com', 'curso' => 'Direito'],
+            ['nome' => 'Ana Paula Souza',      'email' => 'ana.souza@email.com',       'curso' => 'Administração'],
+            ['nome' => 'Pedro Henrique Lima',  'email' => 'pedro.lima@email.com',      'curso' => 'Engenharia Civil'],
+            ['nome' => 'Juliana Costa',        'email' => 'juliana.costa@email.com',   'curso' => 'Engenharia de Software'],
+            ['nome' => 'Rafael Oliveira',      'email' => 'rafael.oliveira@email.com', 'curso' => 'Medicina'],
+            ['nome' => 'Beatriz Almeida',      'email' => 'beatriz.almeida@email.com', 'curso' => 'Direito'],
+            ['nome' => 'Lucas Ferreira',       'email' => 'lucas.ferreira@email.com',  'curso' => 'Administração'],
+            ['nome' => 'Camila Rodrigues',     'email' => 'camila.rodrigues@email.com','curso' => 'Engenharia de Software'],
         ];
 
         foreach ($alunos as $aluno) {

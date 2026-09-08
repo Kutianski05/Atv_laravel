6c8a1941b2ef1d89891b33cd455f2130aee08d21
engs-jefersonpereira-collab <engs-jefersonpereira@camporeal.edu.br>
2026-09-08 11:27:24 -0300

    atividade 21

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/admin', function () {
+    return 'Bem-vindo, admin!';
+})->middleware(['auth', 'role:admin']);
+
 require __DIR__.'/auth.php';

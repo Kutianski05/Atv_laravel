c0413f5931b0c3ced947d7b60d6a350f67151036
João Vitor Kutianski <engs-joaokutianski@camporeal.edu.br>
2026-09-07 21:27:10 -0300

    DESAFIO

---
+++ resources/views/partials/menu.blade.php
@@ -0,0 +1,8 @@
+<nav>
+    <a href="{{ route('home') }}">Home</a> |
+    <a href="{{ route('alunos.index') }}">Alunos</a> |
+    <a href="{{ route('alunos.create') }}">Novo Aluno</a> |
+    <a href="/sobre">Sobre</a> |
+    <a href="/contato">Contato</a>
+</nav>
+<hr>
